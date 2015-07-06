0bbaf4ab440f95d5cc6924a656a34f5392116dc2
Chacha Sikes <chachasikes@gmail.com>
2015-07-06 00:27:32 -0700

    add alert for testing if private browsing mode is on in safari web app

--- js/collections.js
+++ js/collections.js
@@ -27,6 +27,7 @@ var Collections = React.createClass({
     );
   },
 
+// safari error: http://stackoverflow.com/questions/14555347/html5-localstorage-error-with-safari-quota-exceeded-err-dom-exception-22-an
   setHTML5LocalStorage: function() {
     var storedCollections = [
         {key: '17FBVvem0oo_nj3KuwsoUeDJmJ0yuibtkJMkR7-vCEFU', cols: 8, active: true, current: true},
@@ -34,7 +35,20 @@ var Collections = React.createClass({
         {key: '1E949ZFaBbQxiSxBBZMyAIw9KJtHolm0XNsnnQoMjuoM', cols: 7, active: true, current: true},
     ];
 
-    sessionStorage.setItem("whatshould_local_paths", JSON.stringify(storedCollections));
+    // Safari, in Private Browsing Mode, looks like it supports localStorage but all calls to setItem
+    // throw QuotaExceededError. We're going to detect this and just silently drop any calls to setItem
+    // to avoid the entire page breaking, without having to do a check at each usage of Storage.
+    if (typeof localStorage === 'object') {
+        try {
+            sessionStorage.removeItem('whatshould_local_paths');
+            sessionStorage.setItem("whatshould_local_paths", JSON.stringify(storedCollections));
+            
+        } catch (e) {
+            Storage.prototype._setItem = Storage.prototype.setItem;
+            Storage.prototype.setItem = function() {};
+            alert('Your web browser does not support storing settings locally. In Safari, the most common cause of this is using "Private Browsing Mode". Some settings may not save or some features may not work properly for you.');
+        }
+    }
   },
 
   loadHTML5LocalStorage: function() {
