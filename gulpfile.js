4661576104b0746145933f38e39737399b694ad7
Chacha Sikes <chachasikes@gmail.com>
2015-07-15 16:49:21 -0700

    save - compile live reload - working on phantomjs and tests

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,163 @@ var gulp = require('gulp'),
     notify = require('gulp-notify'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
-    del = require('del');
+    del = require('del'),
+    jasmine = require('gulp-jasmine'),
+    jasmineBrowser = require('gulp-jasmine-browser');
  
+
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify'); 
+var gulpif = require('gulp-if');
+var streamify = require('gulp-streamify');
+var notify = require('gulp-notify');
+var cssmin = require('gulp-cssmin');
+var gutil = require('gulp-util');
+var shell = require('gulp-shell');
+var glob = require('glob');
+var jasminePhantomJs = require('gulp-jasmine2-phantomjs');
+
+
+// We create an array of dependencies. These are NPM modules you have
+// installed in node_modules. Think: "require('react')" or "require('underscore')"
+var dependencies = [
+    'react' // react is part of this boilerplate
+];
+
+// Now this task both runs your workflow and deploys the code,
+// so you will see "options.development" being used to differenciate
+// what to do
+var browserifyTask = function (options) {
+
+  /* First we define our application bundler. This bundle is the
+     files you create in the "app" folder */
+    var appBundler = browserify({
+        entries: [options.src], // The entry file, normally "main.js"
+        transform: [reactify], // Convert JSX style
+        debug: options.development, // Sourcemapping
+        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+    });
+
+    /* We set our dependencies as externals of our app bundler.
+     For some reason it does not work to set these in the options above */
+  appBundler.external(options.development ? dependencies : []);
+  
+  /* This is the actual rebundle process of our application bundle. It produces
+    a "main.js" file in our "build" folder. */
+  var rebundle = function () {
+    var start = Date.now();
+    console.log('Building APP bundle');
+    appBundler.bundle()
+      .on('error', gutil.log)
+      .pipe(source('main.js'))
+      .pipe(gulpif(!options.development, streamify(uglify())))
+      .pipe(gulp.dest(options.dest))
+      .pipe(gulpif(options.development, livereload())) // It notifies livereload about a change if you use it
+      .pipe(notify(function () {
+        console.log('APP bundle built in ' + (Date.now() - start) + 'ms');
+      }));
+  };
+
+  /* When we are developing we want to watch for changes and
+    trigger a rebundle */
+  if (options.development) {
+    appBundler = watchify(appBundler);
+    appBundler.on('update', rebundle);
+  }
+  
+  // And trigger the initial bundling
+  rebundle();
+
+  if (options.development) {
+
+    // We need to find all our test files to pass to our test bundler
+    var testFiles = glob.sync('./specs/**/*-spec.js');
+    
+    /* This bundle will include all the test files and whatever modules
+       they require from the application */
+    var testBundler = browserify({
+      entries: testFiles,
+      debug: true,
+      transform: [reactify],
+      cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+    });
+
+    // Again we tell this bundle about our external dependencies
+    testBundler.external(dependencies);
+
+    /* Now this is the actual bundle process that ends up in a "specs.js" file
+      in our "build" folder */
+    var rebundleTests = function () {
+      var start = Date.now();
+      console.log('Building TEST bundle');
+      testBundler.bundle()
+        .on('error', gutil.log)
+        .pipe(source('specs.js'))
+        .pipe(gulp.dest(options.dest))
+        .pipe(livereload()) // Every time it rebundles it triggers livereload
+        .pipe(notify(function () {
+          console.log('TEST bundle built in ' + (Date.now() - start) + 'ms');
+        }));
+    };
+    
+    // We watch our test bundle
+    testBundler = watchify(testBundler);
+    
+    // We make sure it rebundles on file change
+    testBundler.on('update', rebundleTests);
+    
+    // Then we create the first bundle
+    rebundleTests();
+
+    /* And now we have to create our third bundle, which are our external dependencies,
+      or vendors. This is React JS, underscore, jQuery etc. We only do this when developing
+      as our deployed code will be one file with all application files and vendors */
+    var vendorsBundler = browserify({
+      debug: true, // It is nice to have sourcemapping when developing
+      require: dependencies
+    });
+    
+    /* We only run the vendor bundler once, as we do not care about changes here,
+      as there are none */
+    var start = new Date();
+    console.log('Building VENDORS bundle');
+    vendorsBundler.bundle()
+      .on('error', gutil.log)
+      .pipe(source('vendors.js'))
+      .pipe(gulpif(!options.development, streamify(uglify())))
+      .pipe(gulp.dest(options.dest))
+      .pipe(notify(function () {
+        console.log('VENDORS bundle built in ' + (Date.now() - start) + 'ms');
+      }));
+    
+  }
+  
+}
+
+// We also have a simple css task here that you can replace with
+// SaSS, Less or whatever
+var cssTask = function (options) {
+    if (options.development) {
+      var run = function () {
+        gulp.src(options.src)
+          .pipe(concat('main.css'))
+          .pipe(gulp.dest(options.dest));
+      };
+      run();
+      gulp.watch(options.src, run);
+    } else {
+      gulp.src(options.src)
+        .pipe(concat('main.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest(options.dest));   
+    }
+}
+
+
+
+
 // Styles
 gulp.task('styles', function() {
   return sass('src/styles/style.scss')
@@ -31,15 +186,17 @@ gulp.task('styles', function() {
 });
 
 // Scripts
-// crashes - uglify error.
 gulp.task('scripts', function() {
   return gulp.src('src/scripts/**/*.js')
     .pipe(jshint('.jshintrc'))
     .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
+    .pipe(concat('app.js'))
     .pipe(gulp.dest('dist/scripts'))
     .pipe(rename({ suffix: '.min' }))
-//     .pipe(uglify())
+    // .pipe(uglify())  // crashes - uglify error.
+    // .on('error', function(){
+    //   //do whatever here
+    // })
     .pipe(gulp.dest('dist/scripts'))
     .pipe(notify({ message: 'Scripts task complete' }));
 });
@@ -59,16 +216,18 @@ gulp.task('clean', function(cb) {
  
 // Default task
 gulp.task('default', ['clean'], function() {
-    gulp.start('styles', 'scripts', 'images');
+    gulp.start('styles', 'scripts', 'images', 'jasmine');
 });
  
 // Watch
 gulp.task('watch', function() {
  
   // Watch .scss files
+  gulp.watch('src/styles/*.scss', ['styles']);
   gulp.watch('src/styles/**/*.scss', ['styles']);
  
   // Watch .js files
+  gulp.watch('src/scripts/*.js', ['scripts']);
   gulp.watch('src/scripts/**/*.js', ['scripts']);
  
   // Watch image files
@@ -76,8 +235,24 @@ gulp.task('watch', function() {
  
   // Create LiveReload server
   livereload.listen();
- 
+
   // Watch any files in dist/, reload on change
   gulp.watch(['dist/**']).on('change', livereload.changed);
+  // gulp.watch(['spec/**']).on('change', livereload.changed);
  
 });
+
+gulp.task('jasmine', function() {
+  var filesForTest = [ 'src/spec/*.js','src/spec/**/*.js', 'src/spec/*.spec.js','src/spec/**/*.spec.js'];
+  return gulp.src(filesForTest)
+    .pipe(gulp.watch(filesForTest))
+    .pipe(jasmineBrowser.specRunner())
+    .pipe(jasmineBrowser.server({port: 8888}));
+});
+
+// gulp.task('jasmine-phantom', function() {
+//   var filesForTest = [ 'src/spec/*.js','src/spec/**/*.js', 'src/spec/*.spec.js','src/spec/**/*.spec.js'];
+//   return gulp.src(filesForTest)
+//     .pipe(jasmineBrowser.specRunner({console: true}))
+//     .pipe(jasmineBrowser.headless());
+// });
